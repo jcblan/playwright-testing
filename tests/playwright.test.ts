8509a4a4e168af4d0bbf78bedb5f14a3b648500e
JcBlan <blancojulic@gmail.com>
2021-05-05 22:34:47 -0300

    Implementado test El campo es obligatorio

--- tests/playwright.test.ts
+++ tests/playwright.test.ts
@@ -12,4 +12,19 @@ describe('Playwright testing', () => {
         expect(await page.title()).to.equal("Universidad de la Cuenca del Plata -");
         await browser.close();
     });
+    it('Texto debera ser "El campo es obligatorio."', async () => {
+        var texto;
+        const browser = await chromium.launch();
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto('https://www.ucp.edu.ar/contactanos/');
+
+        await page.click('#wpcf7-f79-p43-o1 > form > div.col-md-6.col-md-offset-3 > div:nth-child(6) > div > div > input', {button: "left"});
+        
+        texto = await page.innerText('#wpcf7-f79-p43-o1 > form > div.col-md-6.col-md-offset-3 > div:nth-child(1) > div > span');
+        await browser.close();
+        
+        expect(texto).to.equal("El campo es obligatorio.");
+        
+    });
 });
